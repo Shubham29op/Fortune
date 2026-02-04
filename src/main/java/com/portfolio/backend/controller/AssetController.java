1f55549794596b4b89615bc206bdf87ddd47dea7
Shubham Shah <sns@somaiya.edu>
2026-02-04 09:02:30 +0000

    Update AssetController.java
    resolved error

--- src/main/java/com/portfolio/backend/controller/AssetController.java
+++ src/main/java/com/portfolio/backend/controller/AssetController.java
@@ -2,32 +2,24 @@ package com.portfolio.backend.controller;
 
 import com.portfolio.backend.entity.Asset;
 import com.portfolio.backend.repository.AssetRepository;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/assets")
-@CrossOrigin(origins = "*")
+@RequestMapping("/api/assets") // This defines the URL
+@CrossOrigin(origins = "*")    // This allows your HTML frontend to connect
 public class AssetController {
 
     @Autowired
     private AssetRepository assetRepository;
 
-    @Operation(summary = "Get all assets with pagination")
     @GetMapping
-    public Page<Asset> getAllAssets(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size
-    ) {
-        Pageable pageable = PageRequest.of(page, size);
-        return assetRepository.findAll(pageable);
+    public List<Asset> getAllAssets() {
+        return assetRepository.findAll();
     }
-
 }
